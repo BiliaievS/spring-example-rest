ae7c2656c38f7a8ad8a7edecf8e0b9dc40ae5c34
BiliaievS <sbiliaiev@gmail.com>
2023-08-21 23:16:09 +0200

    cleanup

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into customer (name) values ('Sergii');
-insert into customer (name) values ('Josh');
-insert into customer (name) values ('Mateusz');
-insert into customer (name) values ('Tal');
